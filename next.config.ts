5605c18bee7c3ca37f0a925fdf80d12e701396c9
akalanka <shenalakalanka513@gmail.com>
2025-01-22 19:05:45 +0530

    update config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
+  output: "export",
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
+  // Add basePath and assetPrefix for GitHub Pages deployment
+  basePath: "/Nexus-Blog", // Matches your repository name
+  assetPrefix: "/Nexus-Blog", // Ensures assets are correctly loaded from the subpath
 };
 
 export default nextConfig;
