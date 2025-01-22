8773d488ead02ca134e307cafbdd380659194719
Akalanka-00 <Akalanka-00@users.noreply.github.com>
2025-01-22 13:18:26 +0000

    deploy: 639e51ef2209c764bec62eefbb889789f606a13b

--- src/components/categoryList/CategoryList.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import styles from './categoryList.module.css'
-
-export default function CategoryList () {
-  return (
-    <div className={styles.container}>categoryList</div>
-  )
-}
