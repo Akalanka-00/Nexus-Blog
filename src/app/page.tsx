de91f51578824948bcea8fe7507eed8e504dd7c2
Akalanka-00 <Akalanka-00@users.noreply.github.com>
2025-01-22 13:00:09 +0000

    deploy: 069bbb2adbf358c37411e2e64bf3cb6252551df7

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import styles from "@/app/homepage.module.css"
+import CardList from "@/components/cardlist/Cardlist";
+import CategoryList from "@/components/categoryList/CategoryList";
+import Featured from "@/components/featured/Featured";
+import Menu from "@/components/menu/Menu";
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Featured/>
+      <CategoryList/>
+      <div className={styles.content}>
+        <CardList/>
+        <Menu/>
+      </div>
+    </div>
+  );
+}
