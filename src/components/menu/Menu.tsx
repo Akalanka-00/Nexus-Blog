fc9098e46afdea6502681b635dd0499262ffc1fe
Akalanka-00 <Akalanka-00@users.noreply.github.com>
2025-01-23 07:50:42 +0000

    deploy: c616da9c2346ba89dc93cd78e71e52aa958e2044

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,35 +1,25 @@
 import React from 'react'
 import styles from './menu.module.css'
-import Link from 'next/link'
-import Image from 'next/image'
-import { ImageLoader } from '@/utils/ImageLoader'
-import categoryList from '@/data/CategoryData'
-import menuData from '@/data/MenuData'
+import MenuPosts from '../menuPosts/MenuPosts'
+import MenuCategories from '../menuCategories/MenuCategories'
 
 export default function Menu () {
   return (
     <div className={styles.container}>
       <h2 className={styles.subtitle}>{"What's hot"}</h2>
       <h1 className={styles.title}>Most Popular</h1>
-      <div className={styles.items}>
-        {menuData.map((menu, index) => (
+      <MenuPosts withImage= {false} />
 
-          <Link href={menu.link} className={styles.item} key={index}>
-          <div className={styles.imageContainer}>
-            <Image src={ImageLoader(menu.image)} alt="" fill className={styles.image} />
-          </div>
-          <div className={styles.textContainer}>
-            <span className={styles.category} 
-            style={{backgroundColor:categoryList.filter((category)=>category.name===menu.category)[0].categoryColor}}>{menu.category}</span>
-            <h3 className={styles.postTitle}>{menu.title}</h3>
-            <div className={styles.detail}>
-            <span className={styles.username}>{menu.username}</span>
-            <span className={styles.date}> - {menu.username}</span>
-          </div>
-          </div>
-          </Link>
-        ))}
-      </div>
+      <h2 className={styles.subtitle}>{"Discover by Topic"}</h2>
+      <h1 className={styles.title}>Categories</h1>
+      <MenuCategories/>
+
+
+      <h2 className={styles.subtitle}>{"Choosen By Editor"}</h2>
+      <h1 className={styles.title}>Editors Pick</h1>
+      <MenuPosts withImage= {true} />
+
+
     </div>
   )
 }
---
+++ src/components/menuPosts/MenuPosts.tsx
@@ -0,0 +1,33 @@
+import menuData from '@/data/MenuData'
+import Link from 'next/link'
+import React from 'react'
+import styles from "./menuPosts.module.css"
+import Image from 'next/image'
+import { ImageLoader } from '@/utils/ImageLoader'
+import categoryList from '@/data/CategoryData'
+
+const MenuPosts = ({ withImage }: { withImage: boolean }) => {
+  return (
+    <div className={styles.items}>
+        {menuData.map((menu, index) => (
+
+          <Link href={menu.link} className={styles.item} key={index}>
+          {withImage && <div className={styles.imageContainer}>
+            <Image src={ImageLoader(menu.image)} alt="" fill className={styles.image} />
+          </div>}
+          <div className={styles.textContainer}>
+            <span className={styles.category} 
+            style={{backgroundColor:categoryList.filter((category)=>category.name===menu.category)[0].categoryColor}}>{menu.category}</span>
+            <h3 className={styles.postTitle}>{menu.title}</h3>
+            <div className={styles.detail}>
+            <span className={styles.username}>{menu.username}</span>
+            <span className={styles.date}> - {menu.username}</span>
+          </div>
+          </div>
+          </Link>
+        ))}
+      </div>
+  )
+}
+
+export default MenuPosts
