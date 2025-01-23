1a3e0b817df616aafa00d39e8805b119f22666e7
akalanka <shenalakalanka513@gmail.com>
2025-01-23 11:32:56 +0530

    Menu Items | Basic UI

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,8 +1,35 @@
 import React from 'react'
 import styles from './menu.module.css'
+import Link from 'next/link'
+import Image from 'next/image'
+import { ImageLoader } from '@/utils/ImageLoader'
+import categoryList from '@/data/CategoryData'
+import menuData from '@/data/MenuData'
 
 export default function Menu () {
   return (
-    <div className={styles.container}>Menu</div>
+    <div className={styles.container}>
+      <h2 className={styles.subtitle}>{"What's hot"}</h2>
+      <h1 className={styles.title}>Most Popular</h1>
+      <div className={styles.items}>
+        {menuData.map((menu, index) => (
+
+          <Link href={menu.link} className={styles.item} key={index}>
+          <div className={styles.imageContainer}>
+            <Image src={ImageLoader(menu.image)} alt="" fill className={styles.image} />
+          </div>
+          <div className={styles.textContainer}>
+            <span className={styles.category} 
+            style={{backgroundColor:categoryList.filter((category)=>category.name===menu.category)[0].categoryColor}}>{menu.category}</span>
+            <h3 className={styles.postTitle}>{menu.title}</h3>
+            <div className={styles.detail}>
+            <span className={styles.username}>{menu.username}</span>
+            <span className={styles.date}> - {menu.username}</span>
+          </div>
+          </div>
+          </Link>
+        ))}
+      </div>
+    </div>
   )
 }
--- src/data/CategoryData.ts
+++ src/data/CategoryData.ts
@@ -4,36 +4,47 @@ const categoryList = [
       image: "/images/style.png",
       link: "/blog?cat=style",
       bg: "#57c4ff31",
+      categoryColor: "#789cff",
+
     },
     {
       name: "fashion",
       image: "/images/fashion.png",
       link: "/blog?cat=fashion",
       bg: "#da85c731",
+      categoryColor: "#ff7887",
+
     },
     {
       name:"food",
       image:"/images/food.png",
       link:"/blog?cat=food",
-      bg:"#7fb88133"
+      bg:"#7fb88133",
+      categoryColor: "#7fb881",
+
     },
     {
       name:"travel",
       image:"/images/travel.png",
       link:"/blog?cat=travel",
-      bg:"#ff795736"
+      bg:"#ff795736",
+      categoryColor: "#ff7857",
     },
     {
       name: "culture",
       image: "/images/culture.png",
       link: "/blog?cat=culture",
       bg: "#ffb04f45",
+      categoryColor: "#ffb14f",
+
     },
     {
       name:"coding",
       image:"/images/coding.png",
       link:"/blog?cat=coding",
-      bg:"#5e4fff31"
+      bg:"#5e4fff31",
+      categoryColor: "#775aec",
+
     }
   ];
 
