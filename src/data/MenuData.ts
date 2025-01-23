54d7659e72c2321053b87ad8f51ac18af885e5a5
Akalanka-00 <Akalanka-00@users.noreply.github.com>
2025-01-23 06:04:00 +0000

    deploy: dc91d6d880d1e28687dc3acbddc9e3b1fdcef704

---
+++ src/data/MenuData.ts
@@ -0,0 +1,22 @@
+
+const menuData =[
+    {
+        title: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        image: "/images/p1.jpeg",
+        link: "/",
+        category: "travel",
+        username: "John Doe",
+        date: "10.3.2023"
+    },
+
+    {
+        title: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        image: "/images/p1.jpeg",
+        link: "/",
+        category: "coding",
+        username: "John Doe",
+        date: "10.3.2023"
+    }
+];
+
+export default menuData;
