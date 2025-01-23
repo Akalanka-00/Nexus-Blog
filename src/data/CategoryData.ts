b47778bfe20f50d47c3ec78e1ca635f1120b4c5a
akalanka <shenalakalanka513@gmail.com>
2025-01-23 10:28:27 +0530

    Update categories

---
+++ src/data/CategoryData.ts
@@ -0,0 +1,40 @@
+const categoryList = [
+    {
+      name: "style",
+      image: "/images/style.png",
+      link: "/blog?cat=style",
+      bg: "#57c4ff31",
+    },
+    {
+      name: "fashion",
+      image: "/images/fashion.png",
+      link: "/blog?cat=fashion",
+      bg: "#da85c731",
+    },
+    {
+      name:"food",
+      image:"/images/food.png",
+      link:"/blog?cat=food",
+      bg:"#7fb88133"
+    },
+    {
+      name:"travel",
+      image:"/images/travel.png",
+      link:"/blog?cat=travel",
+      bg:"#ff795736"
+    },
+    {
+      name: "culture",
+      image: "/images/culture.png",
+      link: "/blog?cat=culture",
+      bg: "#ffb04f45",
+    },
+    {
+      name:"coding",
+      image:"/images/coding.png",
+      link:"/blog?cat=coding",
+      bg:"#5e4fff31"
+    }
+  ];
+
+  export default categoryList;
